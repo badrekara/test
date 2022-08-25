bc235db11381cb85493d36d29b77aa3682721a41
badrekara <95154307+badrekara@users.noreply.github.com>
2022-08-25 21:04:45 +0100

    commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBfYTwpB8MiYe-I6rcQ8iiVOIOWhfWWsHg",
+    authDomain: "test-1-16a93.firebaseapp.com",
+    projectId: "test-1-16a93",
+    storageBucket: "test-1-16a93.appspot.com",
+    messagingSenderId: "506073616318",
+    appId: "1:506073616318:web:38a467cb56cbba08d173ef"
+  };
+  
