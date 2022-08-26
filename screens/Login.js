8b8eb7b927541853c48a6169ea91432385911f21
badrekara <95154307+badrekara@users.noreply.github.com>
2022-08-26 16:02:10 +0100

    commit

--- screens/Login.js
+++ screens/Login.js
@@ -1,13 +1,61 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { Input, Image, Button } from '@rneui/themed';
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { KeyboardAvoidingView } from 'react-native';
 
 const Login = () => {
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const signIn = () => {
+
+};
+
     return (
-        <View>
-            <Text>hi</Text>
-        </View>
-    )
-}
+        <KeyboardAvoidingView behavior='padding' style={styles.container}>
+            <StatusBar style='light'/>
+            <Image source={{
+                uri:
+                "https://www.f-cdn.com/assets/img/facebook/default-04d56222.jpg",
+            }}
+            style={{ width: 200, height: 200}}
+            />
+            <View style={styles.inputContainer}>
+                 <Input placeholder='Email' 
+                   autoFocus 
+                   type="email" 
+                   value={email} 
+                   onChangeText={(text) => setEmail(text)}/>
+                 <Input placeholder='Password' 
+                   secureTextEntry 
+                   type="password"
+                   value={password} 
+                   onChangeText={(text) => setPassword(text)}/>
+            </View>
+            <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+            <Button containerStyle={styles.button} type="outline" title="Register" />
+            <View style={{ height: 100 }} />
+        </KeyboardAvoidingView>
+    );
+};
 
-export default Login
+export default Login;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        backgroundColor: "white"
+    },
+    inputContainer: {
+        width: 300,
+        
+    },
+    button: {
+        width: 200,
+        marginTop: 10,
+    },
+});
+
